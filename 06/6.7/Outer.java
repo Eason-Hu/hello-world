771baa4debdb9f2d6dd381901ab79e8a7e17f6e6
Eason-Hu <imhu@hotmail.com>
2017-03-28 14:35:14 +0800

    Update Outer.java

--- 06/6.7/Outer.java
+++ 06/6.7/Outer.java
@@ -1,4 +1,21 @@
 public class Outer
 {
-  private 
+  private inｔ outProp = 9;
+  class Inner
+  {
+    private int inProp = 5;
+    public void accessOuterProp()
+    {
+      System.out.println("Outer Class Prop:" + outProp);
+    }
+  }
+  public void accessInnerProp()
+  {
+    System.out.println("Inner Class Prop: " ＋　ｎｅｗ　Iｎｎｅｒ（）.inProp);
+  }
+  public static void main(String[] args)
+  {
+    Outer out = new Outer();
+    out.accessInnerProp();
+  }
 }
